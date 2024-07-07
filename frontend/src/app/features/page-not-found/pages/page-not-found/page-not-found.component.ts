056326fc72126dd029d09b02d0ef36a593a38c0c
Lily <shirley.fox_@hotmail.com>
2024-07-07 13:43:29 +0200

    Create 404 not found page

--- frontend/src/app/features/page-not-found/pages/page-not-found/page-not-found.component.ts
+++ frontend/src/app/features/page-not-found/pages/page-not-found/page-not-found.component.ts
@@ -1,9 +1,16 @@
 import { Component } from '@angular/core';
+import {RouterLink} from "@angular/router";
+import {MatAnchor} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-page-not-found',
   standalone: true,
-  imports: [],
+  imports: [
+    RouterLink,
+    MatAnchor,
+    MatIcon
+  ],
   templateUrl: './page-not-found.component.html',
   styleUrl: './page-not-found.component.scss'
 })
