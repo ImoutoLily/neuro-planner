ab5388ca65502fa44c56118628c70874c3e7f1f6
Lily <shirley.fox_@hotmail.com>
2024-07-07 23:14:39 +0200

    Add visibility toggle to password field on login page

--- frontend/src/app/features/login/pages/login/login.component.ts
+++ frontend/src/app/features/login/pages/login/login.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatError, MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
 import {MatInput} from "@angular/material/input";
 import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 import {LiveErrorStateMatcher} from "../../../../shared/validators/live-error-state-matcher";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-login',
@@ -20,7 +21,10 @@ import {LiveErrorStateMatcher} from "../../../../shared/validators/live-error-st
     MatCardTitle,
     MatButton,
     ReactiveFormsModule,
-    MatError
+    MatError,
+    MatIconButton,
+    MatSuffix,
+    MatIcon
   ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
@@ -30,6 +34,8 @@ export class LoginComponent {
     email: new FormControl("", [Validators.required, Validators.email]),
     password: new FormControl("", [Validators.required, Validators.minLength(12)])
   });
+
+  showPassword = false;
 
   emailMatcher = new LiveErrorStateMatcher();
   passwordMatcher = new LiveErrorStateMatcher();
