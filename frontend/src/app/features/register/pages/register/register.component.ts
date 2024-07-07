8e8cd333cd2f68aaa0f8a7a3a64250ef9dde24bc
Lily <shirley.fox_@hotmail.com>
2024-07-07 23:11:21 +0200

    Create register page

--- frontend/src/app/features/register/pages/register/register.component.ts
+++ frontend/src/app/features/register/pages/register/register.component.ts
@@ -1,12 +1,62 @@
 import { Component } from '@angular/core';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators
+} from "@angular/forms";
+import {MatError, MatFormField, MatLabel, MatSuffix} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {LiveErrorStateMatcher} from "../../../../shared/validators/live-error-state-matcher";
+import {MatButton, MatIconButton} from "@angular/material/button";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardTitle,
+    MatCardContent,
+    ReactiveFormsModule,
+    MatFormField,
+    MatLabel,
+    MatInput,
+    MatError,
+    MatButton,
+    MatCardHeader,
+    MatIconButton,
+    MatIcon,
+    MatSuffix
+  ],
   templateUrl: './register.component.html',
   styleUrl: './register.component.scss'
 })
 export class RegisterComponent {
+  registerForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, this.lengthRangeValidator])
+  });
 
+  showPassword = false;
+
+  emailMatcher = new LiveErrorStateMatcher();
+  passwordMatcher = new LiveErrorStateMatcher();
+
+  register(): void {
+    console.warn("NOT IMPLEMENTED");
+  }
+
+  private lengthRangeValidator(control: AbstractControl): ValidationErrors | null {
+    const value = control.value;
+
+    if (value.length < 12 || value.length > 128) {
+      return { length: true }
+    }
+
+    return null;
+  }
 }
