0ccc53c512440efce7310c7a0df6a7079d5b0926
Lily <shirley.fox_@hotmail.com>
2024-07-07 17:15:26 +0200

    Create login page

--- frontend/src/app/features/login/pages/login/login.component.ts
+++ frontend/src/app/features/login/pages/login/login.component.ts
@@ -1,12 +1,36 @@
 import { Component } from '@angular/core';
+import {MatError, MatFormField, MatLabel} from "@angular/material/form-field";
+import {MatInput} from "@angular/material/input";
+import {MatCard, MatCardActions, MatCardContent, MatCardHeader, MatCardTitle} from "@angular/material/card";
+import {MatButton} from "@angular/material/button";
+import {FormControl, FormGroup, ReactiveFormsModule, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [
+    MatLabel,
+    MatFormField,
+    MatInput,
+    MatCard,
+    MatCardHeader,
+    MatCardActions,
+    MatCardContent,
+    MatCardTitle,
+    MatButton,
+    ReactiveFormsModule,
+    MatError
+  ],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
 export class LoginComponent {
+  loginForm = new FormGroup({
+    email: new FormControl("", [Validators.required, Validators.email]),
+    password: new FormControl("", [Validators.required, Validators.minLength(8)])
+  });
 
+  login(): void {
+    console.warn("NOT IMPLEMENTED");
+  }
 }
