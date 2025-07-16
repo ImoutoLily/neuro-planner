642daf91ea01440410c0fdea984f5dad2a92d5f7
Lily Vandenbore <keanu.vandenbore007@hotmail.com>
2025-07-16 22:59:02 +0200

    Remove unused import

--- frontend/src/app/features/notes/services/notes.service.ts
+++ frontend/src/app/features/notes/services/notes.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject } from "rxjs";
 import { NoteNode } from "@features/notes/models/note-node";
 import { ViewMode } from "@features/notes/models/view-mode";
 
